f4d4de5f4940ee5f801f69bdcbee49294b3b7e66
Michael Bunton <mbnt84@gmail.com>
2021-03-15 01:10:29 -0500

    doing work on the javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert("Hi");
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "c6757dccf0msh5fc585693ca57acp1ff336jsn8d39639ff216",
+		"x-rapidapi-host": "quotes15.p.rapidapi.com"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
