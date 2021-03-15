c39c71ac843d8f45dd4123865defdd1666336171
Michael Bunton <mbnt84@gmail.com>
2021-03-15 17:59:15 -0500

    Its time to turn it in. I can not believe i finished this project.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,54 @@
-fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+var quoteEl= document.getElementsByClassName("quoteOfTheDay");
+var quoteofthedayauthorEl = document.getElementById("quoteAuthor");
+var quoteofthedayquoteEl = document.getElementById("quoteText");
+var horoscopeSignEl = document.getElementById("signDate");
+var horoscopeMessageEl = document.getElementById("signMessage")
+
+var getApi = function (){
+fetch("https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "c6757dccf0msh5fc585693ca57acp1ff336jsn8d39639ff216",
-		"x-rapidapi-host": "quotes15.p.rapidapi.com"
+		"x-rapidapi-host": "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com"
 	}
 })
 .then(response => {
-	console.log(response);
+	return response.json();
+})
+.then(data => {
+    var quoteAuthor= data.author;
+    var quoteText= data.text;
+    quoteofthedayauthorEl.textContent = quoteAuthor;
+    quoteofthedayquoteEl.textContent = quoteText;
+    
+    
 })
 .catch(err => {
 	console.error(err);
 });
+}
+getApi();
+var getApi2 = function (){
+    fetch("https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today", {
+	"method": "POST",
+	"headers": {
+		"x-rapidapi-key": "c6757dccf0msh5fc585693ca57acp1ff336jsn8d39639ff216",
+		"x-rapidapi-host": "sameer-kumar-aztro-v1.p.rapidapi.com"
+	}
+})
+.then(response => {
+	return response.json();
+})
+.then(data => {
+    console.log(data);
+    var signDte = data.current_date;
+    var signDescription= data.description;
+    horoscopeSignEl.textContent = signDte;
+    horoscopeMessageEl.textContent = signDescription;
+
+})
+.catch(err => {
+	console.error(err);
+});
+}
+getApi2();
